c3c08ad5b8e5aafba3b4e0cd95ff9a77f401c538
Nightmare2043 <ash.official2043@gmail.com>
2025-05-03 13:02:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -717,6 +717,11 @@ def logout():
     return redirect(url_for('index'))
 
 
-if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=5000)
+#if __name__ == "__main__":
+    #app.secret_key = os.urandom(12)
+    #app.run(debug=True,host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable if available
+    app.run(host='0.0.0.0', port=port, debug=True)
